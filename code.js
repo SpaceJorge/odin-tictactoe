b28db23ba821e30a29c59ec75af8cac551133298
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-06-09 19:31:46 -0300

    Project Started

---
+++ code.js
@@ -0,0 +1,15 @@
+/*
+General Idea:
+-Define an Array as the Game Board
+Choose:
+    -Let each player assign it's own simbol
+    -Assign a player X and the other O
+-Define a PlayerFactory with appropiate functions
+    -Define 2 players.
+-Define a Turn System/Game Flow
+-Define a Point System
+-Define a Function for Rendering the Board to the HTML
+-Define an interface that allows players to click the GRID and place their simbol
+while not allowing placing where its already been done
+*/
+
